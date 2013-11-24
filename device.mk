f3fe57030208fb72f61176c3b3a5937fa8c7f796
Donald Munn <donald.munn@gmail.com>
2013-11-24 03:50:05 +0000

    Use default wallpaper and audio package.

--- device.mk
+++ device.mk
@@ -267,4 +267,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# This is the mako-specific audio package
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
+
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
